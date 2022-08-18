c15e5945d1eedf33fbcfe326766b4b90a6da7c04
phont <phont@arrow-tech.vn>
2022-08-18 17:05:37 +0700

    update thumb

--- 01app/app.py
+++ 01app/app.py
@@ -1,13 +1,14 @@
 from tkinter.font import names
 from unicodedata import name
+from venv import create
 from insert import insert
 import json
 import os
 import hashlib
 from datetime import datetime
+from PIL import Image
 
-
-ignore_folder = ['.git', '01app']
+ignore_folder = ['.git', '01app', '01thumbs']
 git_link = "https://raw.githubusercontent.com/phobrv/tep/main"
 print(hashlib.md5("whatever your string is".encode('utf-8')).hexdigest())
 
@@ -35,18 +36,29 @@ def handleCreatedAt(createStr: str):
     return createStr
 
 
+def create_thumb(path: str, thumb: str):
+    size = (256, 256)
+    pathThumb = "../01thumbs/{}.png".format(thumb)
+    with Image.open(path) as im:
+        im.thumbnail(size)
+        im.save(pathThumb, "PNG")
+    return pathThumb.replace("..", "")
+
+
 def insertImg():
     for dirInfo in os.walk('../'):
         fdName = handleFolderName(dirInfo[0])
         if fdName not in ignore_folder and fdName != '':
             created_at = handleCreatedAt(fdName)
             for img in dirInfo[2]:
-                thumb = "/{}/{}".format(dirInfo[0].replace("../", ""), img)
-                title = hashlib.md5(thumb.encode('utf-8')).hexdigest()
+                content = "/{}/{}".format(dirInfo[0].replace("../", ""), img)
+                title = hashlib.md5(content.encode('utf-8')).hexdigest()
+                thumb = create_thumb("{}/{}".format(dirInfo[0], img), title)
                 payload = json.dumps({
                     "title": title,
                     "slug": title,
                     "thumb": thumb,
+                    "content": content,
                     "excerpt": git_link,
                     "created_at": str(created_at)
                 })
