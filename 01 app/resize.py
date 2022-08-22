c815c7795cc48d47e7b5dde720fe5d4400791bc3
phont <phont@arrow-tech.vn>
2022-08-22 14:04:46 +0700

    update

--- 01 app/resize.py
+++ 01 app/resize.py
@@ -7,34 +7,36 @@ import os
 import hashlib
 from datetime import datetime
 from PIL import Image
+from os.path import exists
 
-ignore_folder = ['.git', '01 app', '01thumbs', "2048", "1920", "800"]
+ignore_img = ['.DS_Store']
 size2048 = (2048, 2048)
 size1920 = (1920, 1920)
 
 
 def resize(imgPath: str, imgName: str, imgSize, imgFolder):
-
     path = "../{}/{}.png".format(imgFolder, imgName)
-    with Image.open(imgPath) as im:
-        im.thumbnail(imgSize)
-        im.save(path, "PNG")
-    return path.replace("..", "")
+    if exists(path) == False:
+        with Image.open(imgPath) as im:
+            im.thumbnail(imgSize)
+            im.save(path, "PNG")
+        return path.replace("..", "")
 
 
 def handleFolderName(name: str):
-    return name.replace("../", "").split("/")[0]
+    return name.replace("../", "").replace("source/", "")
 
 
 def main(size, folder):
     for dirInfo in os.walk('../source'):
         fdName = handleFolderName(dirInfo[0])
-        if fdName not in ignore_folder and fdName != '':
+        if fdName != '':
             for img in dirInfo[2]:
-                content = "/{}/{}".format(dirInfo[0].replace("../", ""), img)
-                imgName = hashlib.md5(content.encode('utf-8')).hexdigest()
-                imgPath = "{}/{}".format(dirInfo[0], img)
-                print(resize(imgPath, imgName, size, folder))
+                if img not in ignore_img:
+                    content = "/{}/{}".format(dirInfo[0].replace("../", ""), img)
+                    imgName = hashlib.md5(content.encode('utf-8')).hexdigest()
+                    imgPath = "{}/{}".format(dirInfo[0], img)
+                    print(resize(imgPath, imgName, size, folder))
 
 
 main(size1920, '1920')
